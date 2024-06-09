26cd298c415c0c712702c75a536cbb9acf52e3a0
Menka Roy <menkaroy53@gmail.com>
2024-06-09 23:46:33 +0530

    logger.js

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,7 +1,35 @@
+// const fs = require("fs");
+// const path = require("path");
+
+// const logFilePath = path.join(__dirname, "../../logs/requests.log");
+// const logStream = fs.createWriteStream(logFilePath, { flags: "a" });
+
+// function loggerMiddleware(req, res, next) {
+//   const start = Date.now();
+
+//   res.on("finish", () => {
+//     const duration = Date.now() - start;
+//     const log = `${req.method} ${req.url} ${res.statusCode} - ${duration}ms\n`;
+//     logStream.write(log);
+//   });
+
+//   next(); // Don't forget to call next() to pass control to the next middleware
+// }
+
+// module.exports = loggerMiddleware;
 const fs = require("fs");
 const path = require("path");
 
-const logFilePath = path.join(__dirname, "../../logs/requests.log");
+// Define the writable log directory and file
+const logDirectory = "/tmp/logs";
+const logFilePath = path.join(logDirectory, "requests.log");
+
+// Ensure the log directory exists
+if (!fs.existsSync(logDirectory)) {
+  fs.mkdirSync(logDirectory, { recursive: true });
+}
+
+// Create a write stream for logging
 const logStream = fs.createWriteStream(logFilePath, { flags: "a" });
 
 function loggerMiddleware(req, res, next) {
